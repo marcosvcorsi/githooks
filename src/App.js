a6659b5fd00d487512f258a238afaefdf1bb0e49
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-01-26 11:49:13 -0300

    Project finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const response = await fetch('https://api.github.com/users/marcosvcorsi/repos');
+      const data = await response.json();
+
+      setRepositories(data);
+    }
+
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    const favorites = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${favorites.length} favoritos`;
+  }, [repositories]);
+
+  function handleAddRepository() {
+    setRepositories([...repositories, { id: Math.random(), name: 'novo-repo' }])
+  }
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo;
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>favoritar</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleAddRepository}>
+        Adicionar repositorio
+      </button>
+    </>
+  )
 }
-
-export default App;
